8365e4a9b43231350da1ed943b18fbb3aceb760e
Jake Stanger <jakestanger@gmail.com>
2017-06-29 15:52:10 +0100

    Tidied code Improved commenting and split funcitons into smaller ones where necessary.

--- boggleSolver.py
+++ boggleSolver.py
@@ -5,13 +5,16 @@ import timeit
 
 app = Flask(__name__)
 
+#Render HTML on root path
 @app.route('/')
 def index(name=None):
     return render_template('index.html',name=name)
 
+#Globals
 board = []
 BOARD_SIZE = 0
 
+#Constants
 ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
 
 #Load database
@@ -19,12 +22,12 @@ db = sql.connect('data/dictionary')
 cursor = db.cursor()
 
 #History
-#words = {}
 adjacentCoords = {}
 adjacentLetters = {}
 letterPositions = {}
 
 def getAllWords():
+	'''Gets all the words from the database.'''
 	words = []
 	cursor.execute("SELECT word FROM words")
 	for row in cursor:
@@ -34,15 +37,15 @@ def getAllWords():
 words = {"words": getAllWords()}
 
 def getWordScore(word):
-    length = len(word)
-    if length <= 4: return 1
-    if length == 5: return 2
-    if length == 6: return 3
-    if length == 7: return 5
-    if length >= 8: return 11
+	'''Gets the score of a word.'''
+	POINTS = {4: 1, 5: 2, 6: 3, 7: 5, 8: 11}
+	length = len(word)
+	if length < 4: length = 4
+	if length > 8: length = 8
+	return POINTS[length]
 
 def getCharPositions(char):
-	'''Gets all the positions of a given character on the board'''
+	'''Gets all the positions of a given character on the board.'''
 	#Check if any of the character actually exist on the board
 	if not any(char in boardRow for boardRow in board):
 		return None
@@ -54,15 +57,15 @@ def getCharPositions(char):
 		return chars
 
 def getLetterAt(coord):
-	'''Gets the character at the given coordinate'''
+	'''Gets the character at the given coordinate.'''
 	return board[coord[0]][coord[1]]
 
 def areAdjacent(coord1, coord2):
-	'''Checks if two coordinates are adjacent'''
+	'''Checks if two coordinates are adjacent.'''
 	return coord2 in getAdjacentCoords(coord1[0], coord1[1])
 
 def getAdjacentCoords(x, y):
-	'''Gets a list of adjacent coordinates for the given set of coordinates'''
+	'''Gets a list of adjacent coordinates for the given set of coordinates.'''
 	if (x, y) in adjacentCoords:
 		return adjacentCoords[(x, y)]
 
@@ -83,7 +86,7 @@ def getAdjacentCoords(x, y):
 	return coords
 
 def getAdjacentChars(x, y):
-	'''Gets a list of adjacent letters for the given set of coordinates'''
+	'''Gets a list of adjacent letters for the given set of coordinates.'''
 	if (x, y) in adjacentLetters:
 		return adjacentLetters[(x, y)]
 
@@ -99,15 +102,29 @@ def getAdjacentChars(x, y):
 	adjacentLetters[(x, y)] = returnData
 	return returnData
 
-def findCharOnBoard(char):
+def getCharLocations(char):
+	'''Gets all the locations of a given character on the board.'''
 	occurs = []
 	for i in range(len(board)):
 		indices = [j for j, x in enumerate(board[i]) if x == char]
 		for k in range(len(indices)): occurs.append((i, indices[k]))
 	return occurs
 
+def isNextCharValid(adjacentChars, word, i, locationHistory):
+	'''Checks if any of the possible positions for the next char are valid.'''
+	isValid = False
+	for nextPos in adjacentChars[word[i+1]]:
+		#Check this location is adjacent
+		if location in getAdjacentCoords(nextPos[0], nextPos[1]):
+			#Check next position has not been used
+			if not nextPos in locationHistory:
+				isValid = True
+
+	return isValid
+
 def isWordValid(word, i, locationHistory, locations):
-	'''Recurs to check if the given word is valid'''
+	'''Recurs to check if the given word is valid
+	i.e whether it can be made given the current board.'''
 	#Crop history length (there seems to be some issue with appending otherwise)
 	locationHistory = locationHistory[:i]
 
@@ -120,19 +137,9 @@ def isWordValid(word, i, locationHistory, locations):
 			if word[i+1] in adjacentChars and (word[i-1] in adjacentChars or i-1 < 0):
 				#Check current location is adjacent to previously added location
 				if len(locationHistory) == 0 or areAdjacent(locationHistory[-1], location):
-
-					#Check all possible positions of next char are valid
-					anyPossibleWay = False
-					for nextPos in adjacentChars[word[i+1]]:
-						#Check this location is adjacent
-						if location in getAdjacentCoords(nextPos[0], nextPos[1]):
-							#Check next position has not been used
-							if not nextPos in locationHistory:
-								anyPossibleWay = True
-
-					#If no tile for the next character is validfgdgf
+					#If no tile for the next character is valid
 					#Then skip to the next location for this character
-					if not anyPossibleWay: continue
+					if not isNextCharValid(adjacentChars, word, i, locationHistory): continue
 
 					#If less than max recursion depth
 					if i < len(word)-2:
@@ -143,22 +150,11 @@ def isWordValid(word, i, locationHistory, locations):
 
 	return anyLocationValid
 
-@app.route('/boggleSolver.py', methods=['GET', 'POST'])
-def boggleSolve():
-	startTime = timeit.default_timer()
-
+def setupBoard():
+	'''Processes the board string from the AJAX request
+	into a 2D array usable in the program.'''
 	global board
 	global BOARD_SIZE
-
-	global words
-	global adjacentCoords
-	global adjacentLetters
-	global letterPositions
-
-	#Clear history
-	adjacentCoords = {}
-	adjacentLetters = {}
-	letterPositions = {}
 
 	data = ast.literal_eval(request.get_data().decode("utf-8"))['board']
 
@@ -169,20 +165,48 @@ def boggleSolve():
 
 	BOARD_SIZE = len(board) #We can assume the board width = height
 
-	results = []
+def findCharPositions():
+	'''Finds the positons of each character on the board.'''
+	global ALPHABET
+	global letterPositions
 
-	#Find position of each character on the board
+	#Clear history
+	letterPositions = {}
 	for letter in ALPHABET:
 		charPositions = getCharPositions(letter)
 		if not charPositions == None:
 			letterPositions[letter] = getCharPositions(letter)
 
-	#Check remaining words to make sure each of their
+def checkWords():
+	validWords = []
+	#Check words to make sure each of their
 	#characters are adjacent.
 	for key in words:
 		for word in words[key]:
-			isValid = isWordValid(word, 0, [], findCharOnBoard(word[0]))
-			if isValid: results.append(word)
+			isValid = isWordValid(word, 0, [], getCharLocations(word[0]))
+			if isValid: validWords.append(word)
+
+	return validWords
+
+
+#--POST request recieved--
+@app.route('/boggleSolver.py', methods=['GET', 'POST'])
+def boggleSolve():
+	startTime = timeit.default_timer()
+
+	global adjacentCoords
+	global adjacentLetters
+
+	#Clear history
+	adjacentCoords = {}
+	adjacentLetters = {}
+
+	#Setup board
+	setupBoard()
+	findCharPositions()
+
+	#Find all words
+	results = checkWords()
 
 	db.commit()
 
