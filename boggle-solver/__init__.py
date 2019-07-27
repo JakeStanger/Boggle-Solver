d0249aaa733fa95b3153dbaa144e56d283434ca0
Jake Stanger <mail@jstanger.dev>
2019-07-27 20:22:34 +0100

    Attempt to restructure to support setuptools

---
+++ boggle-solver/__init__.py
@@ -0,0 +1,1 @@
+from .boggleSolver import *
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='Boggle-Solver',
+    version='1.0.0',
+    packages=['boggle-solver'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Flask', 'sqlite3'],
+    url='https://github.com/JakeStanger/Boggle-Solver',
+    license='MIT',
+    author='Jake Stanger',
+    author_email='mail@jstanger.dev',
+    description='Flask app to find words on a given Boggle board.'
+)
